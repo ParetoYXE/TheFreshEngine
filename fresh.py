0431e326554c157fdab1ff8e5226f8b1ae7aab4c
unknown <paretoyxe@gmail.com>
2020-12-02 02:27:01 -0600

    Added basic render test

--- fresh.py
+++ fresh.py
@@ -24,15 +24,38 @@ screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 
 done = False
 
+demonImg = pygame.image.load("demon.png")
+w, h = pygame.display.get_surface().get_size()
 
+demonX = round(w / 5)
+demonY = round(h / 5)
+demonXScale = 100
+demonYScale = 100
+
+
+
+def renderEntity():
+	global demonImg
+	demonImgNew = pygame.transform.scale(demonImg,(demonXScale,demonYScale))
+	screen.blit(demonImgNew,(demonX,demonY))
 
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
 		if event.type == pygame.KEYDOWN: 
-			done = True
+			if event.key == pygame.K_w:
+				demonXScale +=10
+				demonYScale +=10
+			elif event.key == pygame.K_d:
+				demonX +=10
+			elif event.key == pygame.K_a:
+				demonX -=10
+			elif event.key == pygame.K_ESCAPE:
+				done = True
+
 
 
 	screen.fill([255,255,255])
+	renderEntity()
 	pygame.display.update()
